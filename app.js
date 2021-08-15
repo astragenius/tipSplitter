9eb74b43d85c915f2fc34f834c8eefe3af2a3acf
astragenius <darthmalak86@gmail.com>
2021-08-15 15:48:49 +0200

    add variable for input

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+const inputBill = document.querySelector('.input__bill > input[type=text]').value;
+const inputPerson = document.querySelector('.input__people > input[type=text]').value;
+
+
+
+function calculate (num) {
+
+
+    
+
+    
+}
