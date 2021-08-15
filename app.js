a74a22ad1f1b867e2b24ea39658053e0737fb7a6
astragenius <darthmalak86@gmail.com>
2021-08-15 17:57:09 +0200

    added calculator function and showtip and showTotal function

--- app.js
+++ app.js
@@ -1,15 +1,74 @@
 
 
 
-const inputBill = document.querySelector('.input__bill > input[type=text]').value;
-const inputPerson = document.querySelector('.input__people > input[type=text]').value;
 
 
 
-function calculate (num) {
 
+
+
+function calculate (value) {
+
+   
+    // die höhe des rechnungs betrages
+    // wieviele personen wird der tip betrag geteilt?
+    let inputPerson = document.querySelector('.input__people > input[type=text]').value;
+    let inputBill = document.querySelector('.input__bill > input[type=text]').value;
 
     
+    inputPerson =parseInt(inputPerson);
+    inputBill = parseInt(inputBill);
+
+
+
+    //überprüfen ob im feld inputBill und im feld inputPerson ein wert ist. (if statement)
+            // wenn nicht error function ausführen die eine warnmeldung im inputfelt anzeigt
+            // is dürfen keine 0 im personen feld eingetragen werden.
+
+        // ansonsten calculation ausführen.
+
+    // inputwerte in integer umwandeln
+
+
+    // tipAmount = (inputBill * prozentWert(button) / 100 ) / inputPerson
+
+        let tipAmount = ((inputBill * value) / 100) / inputPerson;
+        showTipAmount(Math.floor(tipAmount));
+        
+
+    // tipAmount im bereich Tip Amount / person anzeigen
+
+
+
+
+
+    // total = (tipAmount + inputBill) / inputPerson
+
+
+        let total = (tipAmount + inputBill) / inputPerson;
+        showTotal(Math.floor(total));
+
+       
+
+    // total im breich Total / person anzeigen
+
+
+}
+
+function showTipAmount(tip) {
+    const outputTip = document.querySelector('#output_tip');
+
+   
+    return outputTip.textContent = "$" + tip.toFixed(2);
 
     
 }
+
+function showTotal(total) {
+
+    const outputTotal = document.querySelector('#output_total');
+
+    return outputTotal.textContent = "$" + total.toFixed(2);
+    
+}
+
